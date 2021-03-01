b74ca1bacd9c04d7294af989f8727f7b61ccfd73
Connor Nelson <Connor@ConnorNelson.com>
2021-03-01 02:22:13 -0700

    Shorten docker image names (mitigate bug)

--- generate_sql.sh
+++ generate_sql.sh
@@ -7,11 +7,11 @@ for category in ${CATEGORIES[@]}; do
     for challenge in $(ls -v $category | grep -v '.*\.c'); do
         path="$category/$challenge"
         if [ -d "$path" ]; then
-            docker_image="pwncollege/pwncollege_kernel_challenge"
+            docker_image="pwncollege_kernel_challenge"
         elif echo $path | grep -q '.*\.ko'; then
-            docker_image="pwncollege/pwncollege_kernel_challenge"
+            docker_image="pwncollege_kernel_challenge"
         else
-            docker_image="pwncollege/pwncollege_challenge"
+            docker_image="pwncollege_challenge"
         fi
         echo "insert into challenges (id, name, description, max_attempts, value, category, type, state) values (${id}, '${challenge}', '', 0, 1, '${category}', 'docker', 'visible');"
         echo "insert into docker_challenges (id, docker_image_name) values (${id}, '${docker_image}');"
